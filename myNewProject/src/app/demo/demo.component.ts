26c14a8116a510522e311e6c76d51a2f48ec6b36
jaswanth-jash <jaswanthg12@gmail.com>
2020-12-07 16:22:59 +0530

    Angular

--- myNewProject/src/app/demo/demo.component.ts
+++ myNewProject/src/app/demo/demo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-demo',
+  selector: '.app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
